600a06a212905b4e6094ee7134a9d9c94e284ba7
Gar Mendiola <gmendiola@shopzilla.com>
2014-03-28 15:59:36 -0700

    added nn training

---
+++ 02-unsupervised-learning/parallel_plot.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import sys
+import matplotlib.pyplot as plt
+from pandas import read_csv
+from pandas.tools.plotting import parallel_coordinates
+
+
+data = read_csv(sys.argv[1])
+parallel_coordinates(data[:1000], 'cluster')
+#plt.xticks(rotation=45)
+plt.show()
